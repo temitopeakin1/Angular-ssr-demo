0e89f18d5e6bfba0c29281613a3425020e276f61
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-03-02 21:43:20 +0100

    first commit

---
+++ src/app/course/course.component.ts
@@ -0,0 +1,53 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {Course} from '../model/course';
+import {CoursesService} from '../services/courses.service';
+import {debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
+import {merge, fromEvent} from 'rxjs';
+import {Lesson} from '../model/lesson';
+import {Meta, Title} from '@angular/platform-browser';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NgIf } from '@angular/common';
+
+
+@Component({
+    selector: 'course',
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.scss'],
+    standalone: true,
+    imports: [NgIf, MatProgressSpinnerModule, MatTableModule]
+})
+export class CourseComponent implements OnInit {
+
+
+    course:Course;
+
+    dataSource: MatTableDataSource<Lesson>;
+
+    displayedColumns= ["seqNo", "description", "duration"];
+
+
+    constructor(
+        private route: ActivatedRoute,
+        private coursesService: CoursesService) {
+
+    }
+
+
+
+    ngOnInit() {
+
+        this.course = this.route.snapshot.data["course"];
+
+        this.dataSource = new MatTableDataSource([]);
+
+        this.coursesService.findAllCourseLessons(this.course.id)
+            .subscribe(lessons => this.dataSource.data = lessons);
+
+    }
+
+
+}
